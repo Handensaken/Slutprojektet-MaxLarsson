a9990a76a504ad871f07ce9636943de2c98bc438
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-03-25 23:17:02 +0100

    Lesson work
    added a flame turret that shoots fire as an obstacle on the secret path.

---
+++ Assets/flameScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= -10)
+        {
+            Destroy(this.gameObject);
+        } 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "FlameTurret" && collision.gameObject.tag != "flame")
+        {
+             Destroy(this.gameObject);
+            Debug.Log("hit " + collision.gameObject.name);
+        }
+    }
+}
---
+++ Assets/scripts/FlameScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class FlameScript : MonoBehaviour
+{
+    float timer = 15f;
+    float timer2 = 0.1f;
+    public GameObject projectile;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 433;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer2 -= Time.deltaTime;
+        timer -= Time.deltaTime;
+        if (timer >= 10)
+        {
+            if (timer2 <= 0)
+            {
+                Fire();
+                timer2 = 0.1f;
+            }
+        }
+        else if (timer <= 0)
+        {
+            timer = 15f;
+        }
+    }
+
+    void Fire()
+    {
+        //Debug.Log("Fired");
+        GameObject clone;
+        clone = Instantiate(projectile, transform.position, Quaternion.identity);
+        clone.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(Vector3.up * speed * Time.deltaTime);
+        Debug.Log(clone.GetComponent<Rigidbody2D>().velocity);
+
+    }
+}
